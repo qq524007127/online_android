8202a9cf82b92ac29508763a8aa442c5b0d00def
shenyunjie <524007127>
2016-04-28 18:04:40 +0800

    修正JSInterface获取当前登录用户账号（userCode)返回值

--- app/src/main/java/cn/com/zhihetech/online/core/view/ZHJSInterface.java
+++ app/src/main/java/cn/com/zhihetech/online/core/view/ZHJSInterface.java
@@ -167,6 +167,7 @@ public class ZHJSInterface implements WebViewUtils.JsInterface {
     @JavascriptInterface
     public void initJsRuntime() {
         getUserId();
+        getUserCode();
         getUserToken();
     }
 
